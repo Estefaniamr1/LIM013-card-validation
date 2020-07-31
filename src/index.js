aaa5b08da84b6ca7de3362b647b95061d3b63a7a
Estefania <estefaniamr716@gmail.com>
2020-07-31 12:01:17 -0500

    agregando funcionalidad de maskify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
 
-console.log(validator);
+let cnumber;
+let valid= document.getElementById('validarNumber');
+valid.addEventListener('click',validarNumber);function validarNumber(){
+
+
+    cnumber = document.getElementById('numberCard').value;
+    let c1= validator.isValid (cnumber);
+    let c2= validator.maskify(cnumber);
+    console.log (c1);
+    console.log (c2);
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,37 @@
 const validator = {
-  // ...
-};
+  
+  isValid: function (cardNumber){
+  cardNumber = cardNumber.split(' ').join("");
+  if (cardNumber.length !== 16) {
+      return false;
+  }
+  var carray = new Array();
+  for (var i = 0; i < cardNumber.length; i++) {
+      console.log (carray)
+  }
+  carray.reverse();
+  var sum = 0;
+  for (var i = 0; i < carray.length; i++) {
+      var tmp = carray[i];
+      if ((i % 2) != 0) {
+          tmp *= 2;
+          if (tmp > 9) {
+              tmp -= 9;
+          }
+      }
+      sum += tmp;
+  }
+  if ((sum % 10) == 0){
+      return true;
+  }
+  else{
+      return false;
+  }
+}, 
+maskify: function(number){
+    let cipher = number.replace(/.(?=.{4,}$)/g, '#');
+    return cipher;
+    }
+    }
 
 export default validator;
